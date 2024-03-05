d81fd0e6ba546c0370108eb8c33d275356bb49f5
Augusto Nunes <augustocesar.udi@gmail.com>
2024-03-05 17:26:58 -0300

    Creating querys in PersonRepository;

--- src/main/java/com/sespmt/practicalproject/repositories/PersonRepository.java
+++ src/main/java/com/sespmt/practicalproject/repositories/PersonRepository.java
@@ -1,8 +1,28 @@
 package com.sespmt.practicalproject.repositories;
 
 import com.sespmt.practicalproject.entities.PersonEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
+import java.util.Optional;
 
 public interface PersonRepository extends JpaRepository<PersonEntity, Long> {
+
+    Page<PersonEntity> findByNameIgnoreCaseContaining(String name, Pageable pageable);
+
+    Page<PersonEntity> findByCpf(String cpf, Pageable pageable);
+
+    Optional<PersonEntity> findByMotherNameIgnoreCase(String motherName);
+
+    @Query("SELECT p FROM PersonEntity p WHERE LOWER(p.name) LIKE LOWER(CONCAT('%', LOWER(:name), '%')) " +
+            "AND (p.birthDate = :birthDate OR :birthDate IS NULL) " +
+            "AND LOWER(p.motherName) LIKE LOWER(CONCAT('%', LOWER(:motherName), '%'))")
+    Page<PersonEntity> searchFiltered(String name,
+                                      LocalDate birthDate,
+                                      String motherName,
+                                      Pageable pageable);
 
 }
