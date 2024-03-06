afdcb9ad92f2dc781be6986b2fa51424ad53bca9
Augusto Nunes <augustocesar.udi@gmail.com>
2024-03-06 18:51:49 -0300

    Adjust AddressService

--- src/main/java/com/sespmt/practicalproject/services/AddressService.java
+++ src/main/java/com/sespmt/practicalproject/services/AddressService.java
@@ -2,12 +2,11 @@ package com.sespmt.practicalproject.services;
 
 import com.sespmt.practicalproject.dto.AddressDto;
 import com.sespmt.practicalproject.entities.AddressEntity;
-import com.sespmt.practicalproject.mapper.AddressMapper;
+import com.sespmt.practicalproject.mapper.AddressConverter;
 import com.sespmt.practicalproject.repositories.AddressRepository;
 import com.sespmt.practicalproject.services.exceptions.DatabaseException;
 import com.sespmt.practicalproject.services.exceptions.ResourceNotFoundException;
 import jakarta.persistence.EntityNotFoundException;
-import org.mapstruct.factory.Mappers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -29,7 +28,7 @@ public class AddressService {
     @Autowired
     private AddressRepository addressRepository;
 
-    private final AddressMapper addressMapper = Mappers.getMapper(AddressMapper.class);
+    private final AddressConverter addressConverter = new AddressConverter();
 
 
     @Transactional(readOnly = true)
@@ -37,13 +36,13 @@ public class AddressService {
 
         List<AddressEntity> searchResult = addressRepository.searchByCity(state, city);
 
-        return searchResult.stream().map(addressMapper::toDto).collect(Collectors.toList());
+        return searchResult.stream().map(addressConverter::toDto).collect(Collectors.toList());
     }
 
     @Transactional(readOnly = true)
     public Page<AddressDto> findAllPaged(Pageable pageable) {
         Page<AddressEntity> page = addressRepository.findAll(pageable);
-        return page.map(addressMapper::toDto);
+        return page.map(addressConverter::toDto);
     }
 
     @Transactional(readOnly = true)
@@ -53,14 +52,14 @@ public class AddressService {
 
         List<AddressEntity> entityList = Collections.singletonList(entity);
         Page<AddressEntity> page = new PageImpl<>(entityList, PageRequest.of(0, 10), entityList.size());
-        return page.map(addressMapper::toDto);
+        return page.map(addressConverter::toDto);
     }
 
     @Transactional
     public AddressDto insert(AddressDto dto) {
-        AddressEntity entity = addressMapper.toEntity(dto);
+        AddressEntity entity = addressConverter.toEntity(dto);
         entity = addressRepository.save(entity);
-        return addressMapper.toDto(entity);
+        return addressConverter.toDto(entity);
     }
 
     @Transactional
@@ -70,7 +69,7 @@ public class AddressService {
             AddressEntity entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
             buildAddressToUpdate(dto, entity);
             entity = addressRepository.save(entity);
-            return addressMapper.toDto(entity);
+            return addressConverter.toDto(entity);
         } catch (EntityNotFoundException e) {
             throw new ResourceNotFoundException("Id not found " + id);
         }
